a84d7575af8ed717c19130bc77858b4106d78eaf
Leonardo Gatica <lgatica@protonmail.com>
2017-09-16 22:40:18 -0300

    style(standard): format code to standard

--- src/index.js
+++ src/index.js
@@ -1,50 +1,56 @@
-'use strict';
+'use strict'
 
-const http = require('http');
+const http = require('http')
 
 const parseAmount = data => {
-  return parseFloat(data.replace(/[$.]/g, '').replace(',', '.'));
-};
+  return parseFloat(data.replace(/[$.]/g, '').replace(',', '.'))
+}
 
 module.exports = () => {
   return new Promise((resolve, reject) => {
-    const uri = 'http://indicadoresdeldia.cl/webservice/indicadores.json';
-    http.get(uri, res => {
-      if (res.statusCode !== 200) {
-        reject(new Error(`Request Failed. Status Code: ${res.statusCode}`));
-      } else {
-        let body = '';
-        res.setEncoding('utf8');
-        res.on('data', chunk => body += chunk);
-        res.on('end', () => {
-          const data = JSON.parse(body);
-          const result = {
-            date: new Date(
-              data.date.replace(
-                /(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2})/,
-                '$1-$2-$3T$4:$5:$6-03:00')).toISOString(),
-            holy: {
-              yesterday: data.santoral.ayer,
-              today: data.santoral.hoy,
-              tomorrow: data.santoral.maniana
-            },
-            indicator: {
-              uf: parseAmount(data.indicador.uf),
-              utm: parseAmount(data.indicador.utm),
-              imacec: parseAmount(data.indicador.imacec),
-              ipc: parseFloat(data.indicador.ipc)
-            },
-            restriction: {
-              normal: {
-                today: data.restriccion.normal,
-                tomorrow: data.restriccion.normal_maniana
+    const uri = 'http://indicadoresdeldia.cl/webservice/indicadores.json'
+    http
+      .get(uri, res => {
+        if (res.statusCode !== 200) {
+          reject(new Error(`Request Failed. Status Code: ${res.statusCode}`))
+        } else {
+          let body = ''
+          res.setEncoding('utf8')
+          res.on('data', chunk => {
+            body += chunk
+          })
+          res.on('end', () => {
+            const data = JSON.parse(body)
+            const result = {
+              date: new Date(
+                data.date.replace(
+                  /(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2})/,
+                  '$1-$2-$3T$4:$5:$6-03:00'
+                )
+              ).toISOString(),
+              holy: {
+                yesterday: data.santoral.ayer,
+                today: data.santoral.hoy,
+                tomorrow: data.santoral.maniana
               },
-              catalitic: data.restriccion.catalitico
+              indicator: {
+                uf: parseAmount(data.indicador.uf),
+                utm: parseAmount(data.indicador.utm),
+                imacec: parseAmount(data.indicador.imacec),
+                ipc: parseFloat(data.indicador.ipc)
+              },
+              restriction: {
+                normal: {
+                  today: data.restriccion.normal,
+                  tomorrow: data.restriccion.normal_maniana
+                },
+                catalitic: data.restriccion.catalitico
+              }
             }
-          };
-          resolve(result);
-        });
-      }
-    }).on('error', err => reject(err));
-  });
-};
+            resolve(result)
+          })
+        }
+      })
+      .on('error', err => reject(err))
+  })
+}
--- test/test.js
+++ test/test.js
@@ -1,74 +1,82 @@
-'use strict';
+'use strict'
 
-const expect = require('chai').expect;
-const nock = require('nock');
-const lib = require('../src');
+const expect = require('chai').expect
+const nock = require('nock')
+const lib = require('../src')
 
 describe('indicadoresdeldia', () => {
   describe('server error', () => {
     beforeEach(() => {
-      nock.disableNetConnect();
+      nock.disableNetConnect()
       nock('http://indicadoresdeldia.cl')
         .get('/webservice/indicadores.json')
-        .replyWithError('Server error');
-    });
+        .replyWithError('Server error')
+    })
 
     it('should return server error', done => {
       lib().catch(err => {
-        expect(err.message).to.eql('Server error');
-        done();
-      });
-    });
-  });
+        expect(err.message).to.eql('Server error')
+        done()
+      })
+    })
+  })
 
   describe('bad status code', () => {
     beforeEach(() => {
-      nock.disableNetConnect();
+      nock.disableNetConnect()
       nock('http://indicadoresdeldia.cl')
         .get('/webservice/indicadores.json')
-        .reply(301);
-    });
+        .reply(301)
+    })
 
     it('should return request error', done => {
       lib().catch(err => {
-        expect(err.message).to.eql('Request Failed. Status Code: 301');
-        done();
-      });
-    });
-  });
+        expect(err.message).to.eql('Request Failed. Status Code: 301')
+        done()
+      })
+    })
+  })
 
   describe('good request', () => {
     beforeEach(() => {
-      nock.disableNetConnect();
+      nock.disableNetConnect()
       nock('http://indicadoresdeldia.cl')
         .get('/webservice/indicadores.json')
-        .reply(200, JSON.stringify({
-          date: '2016-11-13 12:30:01',
-          santoral: {ayer: 'Cristian', hoy: 'Diego', maniana: 'Humberto'},
-          indicador: {uf: '$26.283,75', ipc: '0.2', utm: '$46.091,00', imacec: '1,4'},
-          restriccion: {
-            normal: ['no aplica'],
-            normal_maniana: ['no aplica'],
-            catalitico: ['no aplica']
-          }
-        }));
-    });
+        .reply(
+          200,
+          JSON.stringify({
+            date: '2016-11-13 12:30:01',
+            santoral: { ayer: 'Cristian', hoy: 'Diego', maniana: 'Humberto' },
+            indicador: {
+              uf: '$26.283,75',
+              ipc: '0.2',
+              utm: '$46.091,00',
+              imacec: '1,4'
+            },
+            restriccion: {
+              normal: ['no aplica'],
+              normal_maniana: ['no aplica'],
+              catalitico: ['no aplica']
+            }
+          })
+        )
+    })
 
     it('should return valid data', done => {
       lib().then(data => {
-        expect(data.date).to.eql('2016-11-13T15:30:01.000Z');
-        expect(data.holy.yesterday).to.eql('Cristian');
-        expect(data.holy.today).to.eql('Diego');
-        expect(data.holy.tomorrow).to.eql('Humberto');
-        expect(data.indicator.uf).to.eql(26283.75);
-        expect(data.indicator.ipc).to.eql(0.2);
-        expect(data.indicator.utm).to.eql(46091);
-        expect(data.indicator.imacec).to.eql(1.4);
-        expect(data.restriction.normal.today).to.eql(['no aplica']);
-        expect(data.restriction.normal.tomorrow).to.eql(['no aplica']);
-        expect(data.restriction.catalitic).to.eql(['no aplica']);
-        done();
-      });
-    });
-  });
-});
+        expect(data.date).to.eql('2016-11-13T15:30:01.000Z')
+        expect(data.holy.yesterday).to.eql('Cristian')
+        expect(data.holy.today).to.eql('Diego')
+        expect(data.holy.tomorrow).to.eql('Humberto')
+        expect(data.indicator.uf).to.eql(26283.75)
+        expect(data.indicator.ipc).to.eql(0.2)
+        expect(data.indicator.utm).to.eql(46091)
+        expect(data.indicator.imacec).to.eql(1.4)
+        expect(data.restriction.normal.today).to.eql(['no aplica'])
+        expect(data.restriction.normal.tomorrow).to.eql(['no aplica'])
+        expect(data.restriction.catalitic).to.eql(['no aplica'])
+        done()
+      })
+    })
+  })
+})
